a9820c495e83ae67fd92c5676a2df432202e972b
Tami Williams <tami@asktami.com>
2021-03-12 02:33:22 -0500

    updated for cors

--- src/app.js
+++ src/app.js
@@ -19,12 +19,20 @@ app.use(
 	})
 );
 // make so all responses have an Access-Control-Allow-Origin header set to *, so we'll allow requests from all origins
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*');
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*');
+// 	next();
+// });
 
-app.use(cors());
+// app.use(cors());
+
+app.use(
+	cors({
+		credentials: true,
+		origin: 'https://noteful-react-client-asktami.vercel.app',
+	})
+);
+
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 app.use(validateBearerToken);
