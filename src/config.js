5a8a781b8d0a71f710ac6ce0bd60abdcd6e8258e
Tami Williams <tami@asktami.com>
2021-03-13 13:57:15 -0500

    updated

--- src/config.js
+++ src/config.js
@@ -3,7 +3,9 @@ module.exports = {
 	NODE_ENV: process.env.NODE_ENV || 'test',
 	API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
 	TEST_DATABASE_URL:
-		process.env.TEST_DATABASE_URL || 'postgresql://noteful@localhost/note-test',
+		process.env.TEST_DATABASE_URL ||
+		'postgresql://noteful@localhost/IF_YOUR_TEST_DB_URL_NOT_SET_IN_ENV',
 	DATABASE_URL:
-		process.env.DATABASE_URL || 'postgresql://noteful@localhost/note'
+		process.env.DATABASE_URL ||
+		'postgresql://noteful@localhost/IF_YOUR_DB_URL_NOT_SET_IN_ENV',
 };
