bd14703c9eba50d7ea432df50eaa4d984f1cc65b
Tami Williams <tami@asktami.com>
2021-03-17 19:04:29 -0400

    updated for CORS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 
@@ -12,22 +12,23 @@ const noteRouter = require('./note/note-router');
 const folderRouter = require('./folder/folder-router');
 
 const app = express();
-app.use(cors());
+
+// app.use(cors());
 // should allow CORS requests for anywhere but not working
 
 // ALTERNATIVE, if don't use npm cors pacakage, also not working
 // change * to:
 // https://noteful-react-client-asktami.vercel.app/
 
-// app.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	next();
+});
 
 app.use(
 	morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
