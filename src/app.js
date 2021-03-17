78175fd6575faa445e7430fb78c7ccde1cbf48c8
Tami Williams <tami@asktami.com>
2021-03-17 18:45:22 -0400

    updated for CORS

--- src/app.js
+++ src/app.js
@@ -22,15 +22,15 @@ app.use(cors());
 // };
 // app.options(corsOptions, cors());
 
-// app.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	next();
+});
 
 app.use(
 	morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
