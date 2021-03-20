6eb3ff9142d353cc2768a54ae2a008f6ea5bb2cc
Tami Williams <tami@asktami.com>
2021-03-20 15:58:16 -0400

    updated for CORS

--- src/app.js
+++ src/app.js
@@ -90,7 +90,10 @@ ADDED
 
 // from CodeMentor
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Origin',
+		'https://noteful-app-asktami.vercel.app'
+	);
 	// res.header("Access-Control-Allow-Credentials: true")
 	res.header(
 		'Access-Control-Allow-Headers',
